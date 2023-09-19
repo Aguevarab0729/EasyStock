87bbf568d1602152d9e82e729e38d9a5fa8583f4
Angélica Guevara <angelica0729@gmail.com>
2023-09-19 16:28:19 -0500

    estructura pagina web funcional con el dashboard

--- views/blankPage.php
+++ views/blankPage.php
@@ -7,7 +7,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <link rel="stylesheet" href="./views/src/plugins/fontawesome-free/css/all.min.css">
   <link rel="stylesheet" href="./views/src/dist/css/adminlte.min.css">
-  <link rel="stylesheet" href="./views/src/plugins//sweetalert2/">
+  <link rel="stylesheet" href="./views/src/plugins/sweetalert2/">
 </head>
 <body class="hold-transition sidebar-mini">
   <div class="wrapper">
